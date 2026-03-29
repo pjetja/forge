f6c8d13c3915f9e53d6e0ba5822afc339f593da9
Piotr Sowiński <piotr.sowinski@sembo.se>
2026-03-29 20:10:23 +0200

    feat(09-01): install next-intl + i18n infrastructure + translation JSON scaffolds
    - Install next-intl 4.8.3
    - Create src/i18n/constants.ts with SUPPORTED_LOCALES, DEFAULT_LOCALE, LOCALE_COOKIE
    - Create src/i18n/request.ts reading NEXT_LOCALE cookie, merging 4 namespace files
    - Wrap next.config.ts with createNextIntlPlugin()
    - Update root layout: async, dynamic lang attribute, NextIntlClientProvider, latin-ext subset
    - Create global.d.ts TypeScript IntlMessages augmentation
    - Scaffold 8 translation JSON files (pl/en x common/auth/trainer/trainee)

---
+++ src/i18n/constants.ts
@@ -0,0 +1,4 @@
+export const SUPPORTED_LOCALES = ['pl', 'en'] as const;
+export type Locale = (typeof SUPPORTED_LOCALES)[number];
+export const DEFAULT_LOCALE: Locale = 'pl';
+export const LOCALE_COOKIE = 'NEXT_LOCALE';
